b14763cc3b2a8412c82458f009e08cf73447c9a9
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-30 19:38:30 -0300

    realización de modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,29 @@ $(document).ready(function() {
 	
 	data.forEach(function(element){
 		//console.log(element.title);
-		$(".pines").append('<div class="pines__contenedor">'+
+		var infoPines =  '<a href="#" class="open-modal"><div class="pines__contenedor">'+
 								'<div class="pines__tarjeta">'+
-									'<a href="#" id="login-form" class="modal"><img src="dist/img/'+ element.id +'.jpg"></a>'+
+									'<img src="dist/img/'+ element.id +'.jpg">'+
 									'<h3 class="pines__tarjeta--title">' + element.title + '</h3>'+
 									'<h4 class="pines__tarjeta--username"><i class="fa fa-user-circle" aria-hidden="true"></i>'+element.username+ '</h4>'+
 									'<p class="pines__tarjeta--hashtag">' + element.hashtag+'</p>'+
 								'</div>'+
-							'</div>');
+							'</div></a>';
 	});
-	$('#login-form').click(function(event) {
-		$(this).modal();
+
+	infoPines=$(infoPines); //declarar objeto en jquery
+
+	infoPines.data('pin', element); //metodo para que saque de la data saque toda la info  
+
+	$(".pines").append(infoPines);
+
+	infoPines.click(function(){
+		var pin = infoPines.data('pin');
+		console.log(pin);
+		var modal = '<img src="dist/img/1.jpg">';
+
+		$('#modal').modal();
 	});
+
 });
-			
-					
 			
--- dist/js/script.js
+++ dist/js/script.js
@@ -10252,21 +10252,267 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
+/*
+    A simple jQuery modal (http://github.com/kylefox/jquery-modal)
+    Version 0.8.2
+*/
+
+(function (factory) {
+  // Making your jQuery plugin work better with npm tools
+  // http://blog.npmjs.org/post/112712169830/making-your-jquery-plugin-work-better-with-npm
+  if(typeof module === "object" && typeof module.exports === "object") {
+    factory(require("jquery"), window, document);
+  }
+  else {
+    factory(jQuery, window, document);
+  }
+}(function($, window, document, undefined) {
+
+  var modals = [],
+      getCurrent = function() {
+        return modals.length ? modals[modals.length - 1] : null;
+      },
+      selectCurrent = function() {
+        var i,
+            selected = false;
+        for (i=modals.length-1; i>=0; i--) {
+          if (modals[i].$blocker) {
+            modals[i].$blocker.toggleClass('current',!selected).toggleClass('behind',selected);
+            selected = true;
+          }
+        }
+      };
+
+  $.modal = function(el, options) {
+    var remove, target;
+    this.$body = $('body');
+    this.options = $.extend({}, $.modal.defaults, options);
+    this.options.doFade = !isNaN(parseInt(this.options.fadeDuration, 10));
+    this.$blocker = null;
+    if (this.options.closeExisting)
+      while ($.modal.isActive())
+        $.modal.close(); // Close any open modals.
+    modals.push(this);
+    if (el.is('a')) {
+      target = el.attr('href');
+      //Select element by id from href
+      if (/^#/.test(target)) {
+        this.$elm = $(target);
+        if (this.$elm.length !== 1) return null;
+        this.$body.append(this.$elm);
+        this.open();
+      //AJAX
+      } else {
+        this.$elm = $('<div>');
+        this.$body.append(this.$elm);
+        remove = function(event, modal) { modal.elm.remove(); };
+        this.showSpinner();
+        el.trigger($.modal.AJAX_SEND);
+        $.get(target).done(function(html) {
+          if (!$.modal.isActive()) return;
+          el.trigger($.modal.AJAX_SUCCESS);
+          var current = getCurrent();
+          current.$elm.empty().append(html).on($.modal.CLOSE, remove);
+          current.hideSpinner();
+          current.open();
+          el.trigger($.modal.AJAX_COMPLETE);
+        }).fail(function() {
+          el.trigger($.modal.AJAX_FAIL);
+          var current = getCurrent();
+          current.hideSpinner();
+          modals.pop(); // remove expected modal from the list
+          el.trigger($.modal.AJAX_COMPLETE);
+        });
+      }
+    } else {
+      this.$elm = el;
+      this.$body.append(this.$elm);
+      this.open();
+    }
+  };
+
+  $.modal.prototype = {
+    constructor: $.modal,
+
+    open: function() {
+      var m = this;
+      this.block();
+      if(this.options.doFade) {
+        setTimeout(function() {
+          m.show();
+        }, this.options.fadeDuration * this.options.fadeDelay);
+      } else {
+        this.show();
+      }
+      $(document).off('keydown.modal').on('keydown.modal', function(event) {
+        var current = getCurrent();
+        if (event.which == 27 && current.options.escapeClose) current.close();
+      });
+      if (this.options.clickClose)
+        this.$blocker.click(function(e) {
+          if (e.target==this)
+            $.modal.close();
+        });
+    },
+
+    close: function() {
+      modals.pop();
+      this.unblock();
+      this.hide();
+      if (!$.modal.isActive())
+        $(document).off('keydown.modal');
+    },
+
+    block: function() {
+      this.$elm.trigger($.modal.BEFORE_BLOCK, [this._ctx()]);
+      this.$body.css('overflow','hidden');
+      this.$blocker = $('<div class="' + this.options.blockerClass + ' blocker current"></div>').appendTo(this.$body);
+      selectCurrent();
+      if(this.options.doFade) {
+        this.$blocker.css('opacity',0).animate({opacity: 1}, this.options.fadeDuration);
+      }
+      this.$elm.trigger($.modal.BLOCK, [this._ctx()]);
+    },
+
+    unblock: function(now) {
+      if (!now && this.options.doFade)
+        this.$blocker.fadeOut(this.options.fadeDuration, this.unblock.bind(this,true));
+      else {
+        this.$blocker.children().appendTo(this.$body);
+        this.$blocker.remove();
+        this.$blocker = null;
+        selectCurrent();
+        if (!$.modal.isActive())
+          this.$body.css('overflow','');
+      }
+    },
+
+    show: function() {
+      this.$elm.trigger($.modal.BEFORE_OPEN, [this._ctx()]);
+      if (this.options.showClose) {
+        this.closeButton = $('<a href="#close-modal" rel="modal:close" class="close-modal ' + this.options.closeClass + '">' + this.options.closeText + '</a>');
+        this.$elm.append(this.closeButton);
+      }
+      this.$elm.addClass(this.options.modalClass).appendTo(this.$blocker);
+      if(this.options.doFade) {
+        this.$elm.css('opacity',0).show().animate({opacity: 1}, this.options.fadeDuration);
+      } else {
+        this.$elm.show();
+      }
+      this.$elm.trigger($.modal.OPEN, [this._ctx()]);
+    },
+
+    hide: function() {
+      this.$elm.trigger($.modal.BEFORE_CLOSE, [this._ctx()]);
+      if (this.closeButton) this.closeButton.remove();
+      var _this = this;
+      if(this.options.doFade) {
+        this.$elm.fadeOut(this.options.fadeDuration, function () {
+          _this.$elm.trigger($.modal.AFTER_CLOSE, [_this._ctx()]);
+        });
+      } else {
+        this.$elm.hide(0, function () {
+          _this.$elm.trigger($.modal.AFTER_CLOSE, [_this._ctx()]);
+        });
+      }
+      this.$elm.trigger($.modal.CLOSE, [this._ctx()]);
+    },
+
+    showSpinner: function() {
+      if (!this.options.showSpinner) return;
+      this.spinner = this.spinner || $('<div class="' + this.options.modalClass + '-spinner"></div>')
+        .append(this.options.spinnerHtml);
+      this.$body.append(this.spinner);
+      this.spinner.show();
+    },
+
+    hideSpinner: function() {
+      if (this.spinner) this.spinner.remove();
+    },
+
+    //Return context for custom events
+    _ctx: function() {
+      return { elm: this.$elm, $elm: this.$elm, $blocker: this.$blocker, options: this.options };
+    }
+  };
+
+  $.modal.close = function(event) {
+    if (!$.modal.isActive()) return;
+    if (event) event.preventDefault();
+    var current = getCurrent();
+    current.close();
+    return current.$elm;
+  };
+
+  // Returns if there currently is an active modal
+  $.modal.isActive = function () {
+    return modals.length > 0;
+  }
+
+  $.modal.getCurrent = getCurrent;
+
+  $.modal.defaults = {
+    closeExisting: true,
+    escapeClose: true,
+    clickClose: true,
+    closeText: 'Close',
+    closeClass: '',
+    modalClass: "modal",
+    blockerClass: "jquery-modal",
+    spinnerHtml: null,
+    showSpinner: true,
+    showClose: true,
+    fadeDuration: null,   // Number of milliseconds the fade animation takes.
+    fadeDelay: 1.0        // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
+  };
+
+  // Event constants
+  $.modal.BEFORE_BLOCK = 'modal:before-block';
+  $.modal.BLOCK = 'modal:block';
+  $.modal.BEFORE_OPEN = 'modal:before-open';
+  $.modal.OPEN = 'modal:open';
+  $.modal.BEFORE_CLOSE = 'modal:before-close';
+  $.modal.CLOSE = 'modal:close';
+  $.modal.AFTER_CLOSE = 'modal:after-close';
+  $.modal.AJAX_SEND = 'modal:ajax:send';
+  $.modal.AJAX_SUCCESS = 'modal:ajax:success';
+  $.modal.AJAX_FAIL = 'modal:ajax:fail';
+  $.modal.AJAX_COMPLETE = 'modal:ajax:complete';
+
+  $.fn.modal = function(options){
+    if (this.length === 1) {
+      new $.modal(this, options);
+    }
+    return this;
+  };
+
+  // Automatically bind links with rel="modal:close" to, well, close the modal.
+  $(document).on('click.modal', 'a[rel~="modal:close"]', $.modal.close);
+  $(document).on('click.modal', 'a[rel~="modal:open"]', function(event) {
+    event.preventDefault();
+    $(this).modal();
+  });
+}));
+
 var data = [{"id":1,"title":"Fuke zisukje bu dibas sik.","description":"Jusola mud afeafu ti mu.","user":"Ernest Barber","image_url":"1.jpg","username":"nerdale","hashtag":"Chic"},{"id":2,"title":"Pegup netotuv vohemucu tac kipifi.","description":"Samcaro am vazcat bujgumwa no ruden.","user":"Cecilia Sharp","image_url":"2.jpg","username":"carlita","hashtag":"Hairstyles"},{"id":3,"title":"Vek dufpodvo sojuzgid ehi le.","description":"Jazfegmi cemzalsan bi li oti agojoki hiim.","user":"Lucinda Ray","image_url":"3.jpg","username":"nadiatap","hashtag":"Wedding"},{"id":4,"title":"Wisiz bibusbuc giji lesi kebise.","description":"Wetniwop pegniwiv gahcu fok aja mazorut vi ziicnu.","user":"Jose Lyons","image_url":"4.jpg","username":"bosquedeinvierno","hashtag":"Wedding"},{"id":5,"title":"It rij dutu meb jitansef.","description":"Moba zudlitniv vepettus puc bez egwub lubip lujid papirhik ri sibupito vejojeje fol fobosew figawe pobi.","user":"Alice Sharp","image_url":"5.jpg","username":"vale","hashtag":"Flowers"},{"id":6,"title":"Tekcubmu banaroj tupjapti su holkug.","description":"Liszamzak bani jakvu bif suenru cakanum degemap zupek pedojgi kibogu paupuno rarnaj dok hiup jigu.","user":"Theresa Bush","image_url":"6.jpg","username":"nadiatap","hashtag":"Hippie"},{"id":7,"title":"Kivka ohfow atopede lobja tiraveara.","description":"Hufisu suf ekkalic focinzar elo jofoc gemmezdi cunzera.","user":"Caleb Malone","image_url":"7.jpg","username":"nerdale","hashtag":"Decorations"},{"id":8,"title":"Ni vaolu cu ujdu wasumulis.","description":"Tit wozfeafa ku sa likikbaw fu kaglac kirvafzi cagsevaw hope lawbiri jedovku seba.","user":"Connor Bowen","image_url":"8.jpg","username":"vale","hashtag":"Hippie"},{"id":9,"title":"Ujecegez islu tocureti salzup ow.","description":"Pa uce jaw holse mocka mogokobi egued zimmu.","user":"Harriett Frank","image_url":"9.jpg","username":"vreco","hashtag":"Rings"},{"id":10,"title":"Guto juwapan pe zi ejozico.","description":"Toaw vivfo jajgu cujnolsah hul ezogez lelo beji ute mo fujfasmez ol ko.","user":"Louisa Brown","image_url":"10.jpg","username":"gabrielista","hashtag":"Chic"},{"id":11,"title":"Vapmi nuhutu hu pav onidib.","description":"Lefi resawi osne evkocuv hez ehuep.","user":"Sara Jensen","image_url":"11.jpg","username":"bosquedeinvierno","hashtag":"Flowers"},{"id":12,"title":"Uja raf douga zanaco neib.","description":"Nekekub zapatas di fituf teot ge ujzahok soajnag ujvehso bu tumiz nobobsim toocava.","user":"Blanche Alvarez","image_url":"12.jpg","username":"marcia","hashtag":"Photography"},{"id":13,"title":"Kevubpul zoposiv samucli delpote zu.","description":"Vali giguvre pakuzav jusawda vikiw etnugev vawiro orocib bezzaw rigeg firab pek ukfoaze dewji ek.","user":"Lina Torres","image_url":"13.jpg","username":"nadiatap","hashtag":"Ideas"},{"id":14,"title":"Nifikgel zekunap hanag omjilwu pil.","description":"Wecca orgu rarip zoha uptomu herficvo zudpobu za kikunive lon givvotza iwvevi id.","user":"Phoebe Davidson","image_url":"14.jpg","username":"vreco","hashtag":"Dresses"},{"id":15,"title":"Lat olohi pirusoj nocilo ekagu.","description":"Fihdoccab ububiwme juc ve gedda nutu id pu.","user":"Blake Jimenez","image_url":"15.jpg","username":"nadiatap","hashtag":"Planning"},{"id":16,"title":"Ku zipaku lecom olegavu der.","description":"Jidefi kippi at canaben lad ror zefu otasuzo uhuriate metgazo vonnuwed piwov ossaj binmi wauc hoibebub rotu mecwuw.","user":"Alan Adkins","image_url":"16.jpg","username":"dulcefru","hashtag":"Wedding"},{"id":17,"title":"Weokuk leb onovogud lehimkah ukowanrec.","description":"Ceczo ifku kafwuc duh pelge kete jo fa pikbo cuk nur ti jukporpi etil ovsa hocepiw.","user":"Lucille Boyd","image_url":"17.jpg","username":"yugo_opresor","hashtag":"Ceremony"},{"id":18,"title":"Uwapic suw lopgo guvot sud.","description":"Zaza kewuwuz akepife wa rawin komlu jodzow iko sessoih tezkeduf lenlogi ufpab lumjafat zet ruev vor ek.","user":"Lewis Bell","image_url":"18.jpg","username":"gabrielista","hashtag":"Hairstyles"},{"id":19,"title":"Cuvuew bidrada gag gudipu oj.","description":"Galko of kabuzupe kougda pun hek cen vozerolo dafkadbih titekwov imu we zugoso zeblouz upa vut ra mujihici fer.","user":"Luella Banks","image_url":"19.jpg","username":"bosquedeinvierno","hashtag":"Dresses"},{"id":20,"title":"Ihtiz vamheket neaviva wiahu uloka.","description":"Vobu pevcurpum mimjectu demnu.","user":"Andre Jones","image_url":"20.jpg","username":"vreco","hashtag":"Dresses"},{"id":21,"title":"Zim pareih pizten evu tu.","description":"Daekdiv wewisut ze.","user":"Irene Curtis","image_url":"21.jpg","username":"vale","hashtag":"Invitations"},{"id":22,"title":"Vazi vi efde gubi evo.","description":"Gid nilvu.","user":"Bryan Little","image_url":"22.jpg","username":"nerdale","hashtag":"Ideas"},{"id":23,"title":"Uvajopwo cu ogawu dudnip heg.","description":"Birokli fapav zelsedjo lefocsu jicuge kozdof ne efi rav sevukupi ijhef ho ov mutomi gessad bowizoveg vojigtul.","user":"Tyler Horton","image_url":"23.jpg","username":"vale","hashtag":"Hippie"},{"id":24,"title":"Ges kodi cerruw nu emnuc.","description":"Idedet usubke buzti vega use va adufav lade.","user":"Brandon Roy","image_url":"24.jpg","username":"bosquedeinvierno","hashtag":"Cakes"},{"id":25,"title":"Ahu namam abo noashu bichofek.","description":"Iti bugpo sepu jugdifveb con nawkorgav fokzodac purujsa vufhal heusi riszaka fibpuja folozaejo.","user":"Nicholas Powers","image_url":"25.jpg","username":"gabrielista","hashtag":"Hairstyles"},{"id":26,"title":"Visoban depef emcajvul isodib jizu.","description":"Suosuko menasepi ufoogase cufveca ontifet fidahi tubopzom vifkooj dobuh na sezo tepguf cacocoku fenezu wozdiute.","user":"Helena Tyler","image_url":"26.jpg","username":"nadiatap","hashtag":"Photography"},{"id":27,"title":"Iha ni hocoj cicizhu umi.","description":"Desotmiz nivuhielu niv wu ze fe hancifi abidih wad ivehedeb doboguv oc rizumec.","user":"Genevieve Hall","image_url":"27.jpg","username":"dulcefru","hashtag":"Ideas"},{"id":28,"title":"Walzod vovlus le mar co.","description":"Vovfa.","user":"Rose Clayton","image_url":"28.jpg","username":"carlita","hashtag":"Hippie"},{"id":29,"title":"Ci kevvef bipolu ti oha.","description":"Cozdav im fupero oh uk sojnoh rubabe ruefediv.","user":"Calvin Fowler","image_url":"29.jpg","username":"onlyblank","hashtag":"Chic"},{"id":30,"title":"Uldujbu potre ocevi datapci cuftomho.","description":"Selpica ni kawof fi.","user":"Norman Daniel","image_url":"30.jpg","username":"yugo_opresor","hashtag":"Ceremony"},{"id":31,"title":"Win fitle duuh cijahoec oldugli.","description":"Zapo od jomgub kagjoz sa hid fesi ajlec.","user":"Alan Weber","image_url":"31.jpg","username":"bosquedeinvierno","hashtag":"Flowers"},{"id":32,"title":"Opod fic ravi hozieg bupu.","description":"Lek gomek follibmu vemazevaw fiske is vo jok tathavu punnus lahaose manum wicfev lovodo kani seiweici uzakijto abodi.","user":"Gertrude Day","image_url":"32.jpg","username":"marcia","hashtag":"Planning"},{"id":33,"title":"Nulaw iguzizlaf igiof me oh.","description":"Ditpij tare reicva kezovi eje nav ki peb vimupwe eguconaj napcuz zegsabzit peot ib safiz.","user":"Steven Osborne","image_url":"33.jpg","username":"yugo_opresor","hashtag":"Colors"},{"id":34,"title":"Duke wal ih unraksog de.","description":"Rezu fujifta suwuhtu nam ciwoidi ugeku viapeaze wab at ziz pe ra ospalwo vaco.","user":"Ruth Bowers","image_url":"34.jpg","username":"carlita","hashtag":"Wedding"},{"id":35,"title":"Zi ni siwe disepu liwevzev.","description":"Ocat hucoduka aldunte bumot heego jislias fijwupka ve.","user":"Todd Griffith","image_url":"35.jpg","username":"vreco","hashtag":"Hairstyles"},{"id":36,"title":"Ik dothi eszi se tulelu.","description":"Ditgi revsegaw suwa ihiwilfav hecera.","user":"Johnny Fisher","image_url":"36.jpg","username":"carlita","hashtag":"Dresses"},{"id":37,"title":"La wupojovi cod isi op.","description":"Ra.","user":"Elva Stephens","image_url":"37.jpg","username":"vale","hashtag":"Planning"},{"id":38,"title":"Efractij vo pomsu erulakrem mozjedfok.","description":"Jik miwu si huarute jaznuat noica iwriopi ehwosmeg mac zow te coodabom ib gunmous gambi eboco pavegip on miajuom.","user":"Irene Sparks","image_url":"38.jpg","username":"bosquedeinvierno","hashtag":"Photography"},{"id":39,"title":"Vuvil ib zap ul fefulo.","description":"Vaso nemi cu tej jiavpos zes ped abo ka lassivizi hibbite munegu adoisu cuuf.","user":"Clarence Stokes","image_url":"39.jpg","username":"onlyblank","hashtag":"Hairstyles"},{"id":40,"title":"Suzjak sunakvoc tuza zahovu sinsija.","description":"Ecruk ome su buma ruapdit wej onuawi wem pi.","user":"Minerva Welch","image_url":"40.jpg","username":"dulcefru","hashtag":"Colors"}];
 $(document).ready(function() {
 	
 	data.forEach(function(element){
 		//console.log(element.title);
-		$(".pines").append('<div class="pines__contenedor">'+
+		var infoPines =  '<a href="#" class="open-modal"><div class="pines__contenedor">'+
 								'<div class="pines__tarjeta">'+
-									'<a href="#" id="" class="modal"><img src="dist/img/'+ element.id +'.jpg"></a>'+
+									'<img src="dist/img/'+ element.id +'.jpg">'+
 									'<h3 class="pines__tarjeta--title">' + element.title + '</h3>'+
 									'<h4 class="pines__tarjeta--username"><i class="fa fa-user-circle" aria-hidden="true"></i>'+element.username+ '</h4>'+
 									'<p class="pines__tarjeta--hashtag">' + element.hashtag+'</p>'+
 								'</div>'+
-							'</div>');
+							'</div></a>';
+		$(".pines").append(infoPines);
 	});
+
+	$('.open-modal').click(function(){
+		$('.modal').modal();
+	});
+
 });
-			
-					
 			
